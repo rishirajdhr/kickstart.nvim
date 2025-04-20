35ecffe40f72d208b808a5ca58ae8c5fd87f8899
Rishiraj Dhar <rishirajdhr@gmail.com>
2025-04-20 05:41:01 -0400

    add indentation defaults

--- init.lua
+++ init.lua
@@ -98,6 +98,12 @@ vim.g.have_nerd_font = true
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+-- Set default indentation rules
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
+
 -- Make line numbers default
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
